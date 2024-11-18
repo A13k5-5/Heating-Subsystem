c7a4a066f916402900bfb68b8bac46293aceaf9c
Alex Pison <pisonalex@gmail.com>
2024-11-18 17:50:46 +0100

    Now correctly measures temperature in Celsius

--- Heating.ino
+++ Heating.ino
@@ -1,13 +1,26 @@
 int analogPin = A3;
 double val = 0.0; // store the read value
+double constRes = 6660.0;
+double totalVolt = 5.0;
+double b = 4220;
+
+double celsToKelvin(double cels) {
+  return cels + 273.15;
+}
+
+double kelvinToCels(double kelv){
+  return kelv - 273.15;
+}
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  val = 5.0 / 1023.0 * analogRead(analogPin);
-  double r1 = 800 * val / (5 - val)
-  Serial.println(val);
+  val = totalVolt / 1023.0 * analogRead(analogPin);
+  double r1 = (constRes * val) / (totalVolt - val);
+  double temp = kelvinToCels((celsToKelvin(25.0)) * b / (b - celsToKelvin(25.0) * log(10000 / r1)));
+
+  Serial.println(temp);
   delay(100);
 }
