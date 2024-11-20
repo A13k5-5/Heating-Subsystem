8b794c5a90397e73e9199dbfe9d81e02dfafa791
Alex Pison <pisonalex@gmail.com>
2024-11-20 14:06:46 +0100

    Heating not turning off

--- Heating.ino
+++ Heating.ino
@@ -5,10 +5,11 @@ double constRes = 6660.0;
 double totalVolt = 5.0;
 double b = 4220;
 double r1, temp;
+double tempUncertainty = 0.6;
 
 // Writing const
 int transistorBase = 2;
-double targetTemp = 25;
+double targetTemp = 28;
 
 double celsToKelvin(double cels) {
   return cels + 273.15;
@@ -21,8 +22,10 @@ double kelvinToCels(double kelv){
 void controlTemp(double curTemp, double targetTemp){
   if (curTemp < targetTemp){
     digitalWrite(transistorBase, LOW);
+    Serial.println("Heating up");
   } else {
     digitalWrite(transistorBase, LOW);
+    Serial.println("Not heating up");
   }
 }
 
@@ -40,5 +43,5 @@ void loop() {
 
   // Writing
   controlTemp(temp, targetTemp);
-  delay(100);
+  delay(1500);
 }
